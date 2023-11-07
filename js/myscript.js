1130c07e2c76df0019398ae76ad05558b8e79f7c
elisabichiappini <elisabichiappini@gmail.com>
2023-11-07 16:04:25 +0100

    feat: scrittura programma js

--- js/myscript.js
+++ js/myscript.js
@@ -1,28 +1,49 @@
 /********************************************************************
 FILE JAVASCRIPT
 ********************************************************************/
+//codice rigido di Javascript
+'use strict';
+
+const prezzoKm = 0.21;
+const scontoSenior = 40;
+const scontoMinorenni = 20;
 // 1) Input per utente (numero) di chilometri da percorrere;
+const kmInput = Number(prompt('Quanti km vuoi percorrere?'));
+console.log(kmInput);
+
 // 2) Input per utente (numero) dell'età del passeggero;
-// 3) Sono valori validi sono in forma numerica?
-// 4)(condizione)
-// 4a)- Se NO: avvisare errore e ritornoare alla compilazione Input km e età.
-// 4b)- Se SI:
-// 5)Il prezzo del biglietto è definito in base ai km (0.21 € al km):
+const etaInput = Number(prompt ('Quanti hanni hai?'));
+console.log(etaInput);
 
-// 6)Condizione:
-// 6a) - SE x < 18 allora va applicato uno sconto del 20% per i minorenni;
-// 6b) - SE x >= 65 allora va applicato uno sconto del 40% per gli over 65.
+// 3)(condizione) SE SI:
+// 4)Il prezzo del biglietto è definito in base ai km (0.21 € al km):
+if (!isNaN(kmInput) && !isNaN(etaInput)) {
+    let prezzoBiglietto = kmInput * prezzoKm;
+    let scontoBiglietto = 0;
+    //5)(condizione) SE minorenne va applicato uno sconto del 20% per i minorenni:
+    if (etaInput < 18) {
+        scontoBiglietto = (prezzoBiglietto * 100) / scontoMinorenni;
+    //5)(condizione) SE senior va applicato uno sconto del 40% per gli over 65:
+    } else if (etaInput >= 65) {
+        scontoBiglietto = (prezzoBiglietto * 100) / scontoSenior;
+    }
 
-/* 7) MILESTONE 1:
+
+// 5) Stampare in console il prezzoBiglietto finale
+    prezzoBiglietto -= scontoBiglietto;
+    console.log('Il prezzo del biglietto è: ', prezzoBiglietto.toFixed(2));
+} else {
+    console.log('Errore hai digidato dei NotANumber!');
+}
+/* 6) MILESTONE 1:
     Iniziamo implementando il programma senza alcuna estetica: usando esclusivamente 
     due input e un bottone (non stilizzati), realizziamo le specifiche scritte sopra. 
-    La risposta finale (o output) sarà anch’essa da scrivere in console.
-*/
+    La risposta finale (o output) sarà anch’essa da scrivere in console. */
 
-/* 8) MILESTONE 2:
+
+/* 7) MILESTONE 2:
     Solo una volta che il milestone 1 sarà completo e funzionante allora realizzeremo un form
     in pagina in cui l’utente potrà inserire i dati e visualizzare il calcolo finale con il prezzo,
     come da screenshot allegato. Il recap dei dati e l’output del prezzo finale, andranno quindi 
     stampati in pagina (il prezzo dovrà essere formattato con massimo due decimali, 
-    per indicare i centesimi sul prezzo).
-*/
+    per indicare i centesimi sul prezzo). */
