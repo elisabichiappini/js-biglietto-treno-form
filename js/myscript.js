c2b6e91e992017e88f42bf56de6b98e7028af0ca
elisabichiappini <elisabichiappini@gmail.com>
2023-11-08 16:27:51 +0100

    feat: biglietto generato e inserimento dati ms 2

--- js/myscript.js
+++ js/myscript.js
@@ -8,17 +8,37 @@ FILE JAVASCRIPT
     Iniziamo implementando il programma senza alcuna estetica: usando esclusivamente 
     due input e un bottone (non stilizzati), realizziamo le specifiche scritte sopra. 
     La risposta finale (o output) sarà anch’essa da scrivere in console. */
-    // 1) dichiaro e assegno valore campo input che manipolerò, dove saranno inseriti i dati numerici km;
+/* MILESTONE 2:
+    Solo una volta che il milestone 1 sarà completo e funzionante allora realizzeremo un form
+    in pagina in cui l’utente potrà inserire i dati e visualizzare il calcolo finale con il prezzo,
+    come da screenshot allegato. Il recap dei dati e l’output del prezzo finale, andranno quindi 
+    stampati in pagina (il prezzo dovrà essere formattato con massimo due decimali, 
+    per indicare i centesimi sul prezzo). */
+
+
+    // 1) dichiaro e assegno valore campo input che manipolerò, dove saranno inseriti i dati numerici km:
     const elementKmUtente = document.getElementById('km');
     
-    // 2) dichiaro e assegno valore del campo input che manipolerò, dove saranno inseriti i dati numerici età;
+    // 2) dichiaro e assegno valore del campo input che manipolerò, dove saranno inseriti i dati numerici età:
     const elementEtaUtente = document.getElementById('eta');
 
-    // 3) dichiaro e assegno valore del botton per generare calcolo degli input per il costo del biglietto;
+    // 3) dichiaro e assegno valore del botton per generare calcolo degli input per il costo del biglietto:
     const elementGeneraButton = document.getElementById('btn-submit');
 
-    //controllo: stampa quali sono gli elementi da manipolare;
-    console.log(elementKmUtente, elementEtaUtente, elementGeneraButton);
+    // 7) dichiaro e assegno valore campo input che manipolerò, dove saranno inseriti i dati testo nome e cognome utente:
+    const elementIdUtente = document.getElementById('utente-id');
+
+    // controllo: stampa quali sono gli elementi da manipolare;
+    // console.log(elementKmUtente, elementEtaUtente, elementGeneraButton, elementIdUtente);
+
+    // 8) dichiaro e assegno valore al modulo biglietto virtuale che comparità qunado genererò il prezzo biglietto:
+    const elementModuloBigliettoVirtuale = document.getElementById('modulo-biglietto-virtuale');
+    // dichiaro gli altri elementi del biglietto virtuale che dovrò far comparire nel dom:
+    const elementVirtualeIdUtente = document.querySelector('.op-naming');
+    const elementVirtualeOffert = document.querySelector('.op-offert');
+    const elementVirtualeSector = document.querySelector('.op-sector');
+    const elementVirtualeCode = document.querySelector('.op-code');
+    const elementVirtualePrezzoFinale = document.querySelector('.op-prezzo-finale');
 
     // 5) dichiaro e assegno valore del botton per annullare gli input per generare il costo del biglietto;
     const elementAnnullaButton = document.getElementById('btn-annull');
@@ -39,29 +59,43 @@ FILE JAVASCRIPT
         // 6) calcolo biglietto e salvo in variabile il valore sulla base dei dati dichiarati in variabili.
         // cacolo biglietto prezzo intero:
         let prezzoBiglietto = prezzoAlKm * Number(elementKmUtente.value);
-        // condizione per cavolo e scorporo sconto:
+        // 9) dichiaro e assegno alla variabile la tipologia di biglietto per op-offert:
+        let typeOffert = 'Prezzo standard';
+        // 6) condizione per calcolo e scorporo sconto:
         if (elementEtaUtente.value === 'minorenne') {
             // cacolo biglietto prezzo minorenne:
             prezzoBiglietto -= (prezzoBiglietto * percentualeMinorenni) / 100;
+            // 9)riassegno alla variabile nuovo valore scontato minori:
+            typeOffert = 'Prezzo Scontato Minorenni';
         } else if (elementEtaUtente.value === 'senior65') {
             // cacolo biglietto prezzo minorenne:
             prezzoBiglietto -= (prezzoBiglietto * percentualeSenior65) / 100;
+            // 9)riassegno alla variabile nuovo valore scontato senior65:
+            typeOffert = 'Prezzo Scontato Senior 65';
         }
+        // controllo: stampo nome e cognome in console sulla base degli input dell'utente:
+        console.log(elementIdUtente.value);
         // controllo: stampo il prezzo biglietto in console sulla base degli input dell'utente e valori arrotondati a due decimali:
         console.log(prezzoBiglietto.toFixed(2));
+        // controllo: stampo in console la tipologia di biglietto attribuito alla scelta di fascia età:
+        console.log(typeOffert);
+
+        // ASSEGNO VALORI DOM
+        // 10) assegno valore nome utente in Dom
+        elementIdUtente.innerHTML = elementIdUtente.value;
+        // 10) assegno valore tariffa in Dom
+        elementVirtualeOffert.innerHTML = typeOffert;
+        // 10) assegno valore prezzofinale in Dom
+        elementVirtualePrezzoFinale.innerHTML = `${prezzoBiglietto.toFixed(2)} €`
     });
+
+    
 
     // 5) evento: reset bottone che annulla l'inserimento dei valori, riassegnando il valore null a 'eta' e 'vuoto' a 'km' e stampa in console la conferma che sia stata resettato l'input di 'km' e 'eta'
     elementAnnullaButton.addEventListener('click',
-    function() {
-        elementKmUtente.value = '';
-        elementEtaUtente.value = null;
-        console.log('annullo i bottoni');
+        function() {
+            elementIdUtente.value = '';
+            elementKmUtente.value = '';
+            elementEtaUtente.value = null;
+            console.log('annullo i bottoni');
     });
-
-/* MILESTONE 2:
-    Solo una volta che il milestone 1 sarà completo e funzionante allora realizzeremo un form
-    in pagina in cui l’utente potrà inserire i dati e visualizzare il calcolo finale con il prezzo,
-    come da screenshot allegato. Il recap dei dati e l’output del prezzo finale, andranno quindi 
-    stampati in pagina (il prezzo dovrà essere formattato con massimo due decimali, 
-    per indicare i centesimi sul prezzo). */
